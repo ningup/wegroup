58f037f206fb0069de328d86985f313d6a6e6963
liningup <ningliup@126.com>
2015-09-14 13:10:08 +0800

    search feature

--- routes/group.js
+++ routes/group.js
@@ -9,30 +9,25 @@ var GroupClass = require('../common/group_class.js'); //引入group_class.js
 //声明一个Group类，为避免堆栈溢出，放到全局变量里面
 var Group = AV.Object.extend('Group');
 
-// 查询 Groups 列表	
+// 搜索 Groups 结果
 router.get('/', function(req, res, next) {
-
-  var query = new AV.Query(Group);
-  query.descending('createdAt');
-  query.find({
-    success: function(results) {
-      res.render('group', {
+  var querys = new AV.SearchQuery(Group);
+  var searchString = req.query.searchString;
+   if(searchString === 'all'){
+  	querys.queryString('*');
+   }
+   else {
+	querys.queryString('*'+searchString+'*');
+   }
+  querys.find().then(function(results) {
+    console.log('Found %d objects', querys.hits());
+    //Process results
+    res.render('group', {
         title: 'Groups 列表',
-	username: req.query.username,
+        username: req.query.username,
         groups: results
       });
-    },
-    error: function(err) {
-      if (err.code === 101) {
-        // 该错误的信息为：{ code: 101, message: 'Class or object doesn\'t exists.' }，说明 Todo 数据表还未创建，所以返回空的 Todo 列表。
-          res.render('group', {
-	  title: 'Groups 列表',
-          groups: []
-        });
-      } else {
-        next(err);
-      }
-    }
+
   });
 });
 
@@ -40,20 +35,24 @@ router.get('/', function(req, res, next) {
 router.post('/create', function(req, res, next) {
   var nickName=req.body.nickName;
   var groupclass = new GroupClass();
+  var searchString = 'all';
   console.log(req.query.username);
   groupclass.create(nickName,req.query.username);       //调用groupclass的建群函数 
-  res.redirect('/group?username='+req.query.username);
+ // res.redirect('/group?username='+req.query.username);
+  //res.redirect('/group?username='+req.query.username+'&searchString=all');
+   res.redirect('/group?username='+req.query.username+'&searchString='+searchString);
 })
 
 
-//用户加入群
+//查询群
 router.post('/follow',function(req,res,next){
- 	var groupName=req.body.targetGroup;
+ 	var searchString=req.body.targetGroup;
 	var username = req.query.username;
 	var groupclass = new GroupClass();
-	groupclass.addFollower(groupName,req.query.username); //添加群成员 
-	groupclass.joinGroup(groupName,req.query.username);  //在用户groupJoined添加群信息
-  	res.redirect('/group?username='+req.query.username);
+
+	//groupclass.addFollower(groupName,req.query.username); //添加群成员 
+	//groupclass.joinGroup(groupName,req.query.username);  //在用户groupJoined添加群信息
+  	res.redirect('/group?username='+req.query.username+'&searchString='+searchString);
 
 })
 
