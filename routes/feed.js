506455bb0068400bc4bc1a05617d7df6419ca415
liningup <ningliup@126.com>
2015-12-03 22:55:54 +0800

    band

--- routes/feed.js
+++ routes/feed.js
@@ -54,63 +54,63 @@ router.get('/', function(req, res, next) {
 	var userclass = new UserClass();
 	client.getAccessToken(req.query.code, function (err, result) {
 		if(err){
-			 //res.redirect('/group/fini?title=');		
-			 var username = 'orSEhuNxAkianv5eFOpTJ3LXWADE';
-			 userclass.getCurrentGroup(username,function(err,whichGroupNow,whichGroupNameNow){
-				 if(err){
-					 res.send('你还没有加入群呢，快去创建一个吧！');
-				 }
-				 else{
-					var groupObjId = whichGroupNow;
-					var query = new AV.Query('Group');
-					query.get(groupObjId, {
-					  success: function(group) {
-						// 成功获得实例
-						 //console.log('you get into the '+ group.get('nickname'));
-						 var relation = group.relation("feedPosted");
-						 relation.targetClassName = 'Feed';
-						 var queryFeed = relation.query();
-						 queryFeed.notEqualTo('feedType','vote');
-						 queryFeed.descending('updateTime');
-						 queryFeed.limit(5);
-						 //queryFeed.equalTo("feedType", "vote");
-						 queryFeed.find().then(function(feeds){
-						   userclass.getUserObj(username,function(err,user){
-							 userclass.getSignInCnt(username,groupObjId, function(err,cnt,isSignIn){
-								//console.log('groupnickname',nickname
-								var loadFeedTime = new Object();
-								if(feeds.length==0){
-									loadFeedTime.latest = new Date();
-									loadFeedTime.oldest = new Date();
+			 res.redirect('/group/fini?title=');		
+			 //var username = 'orSEhuNxAkianv5eFOpTJ3LXWADE';
+			 //userclass.getCurrentGroup(username,function(err,whichGroupNow,whichGroupNameNow){
+				 //if(err){
+					 //res.send('你还没有加入群呢，快去创建一个吧！');
+				 //}
+				 //else{
+					//var groupObjId = whichGroupNow;
+					//var query = new AV.Query('Group');
+					//query.get(groupObjId, {
+					  //success: function(group) {
+						//// 成功获得实例
+						 ////console.log('you get into the '+ group.get('nickname'));
+						 //var relation = group.relation("feedPosted");
+						 //relation.targetClassName = 'Feed';
+						 //var queryFeed = relation.query();
+						 //queryFeed.notEqualTo('feedType','vote');
+						 //queryFeed.descending('updateTime');
+						 //queryFeed.limit(5);
+						 ////queryFeed.equalTo("feedType", "vote");
+						 //queryFeed.find().then(function(feeds){
+						   //userclass.getUserObj(username,function(err,user){
+							 //userclass.getSignInCnt(username,groupObjId, function(err,cnt,isSignIn){
+								////console.log('groupnickname',nickname
+								//var loadFeedTime = new Object();
+								//if(feeds.length==0){
+									//loadFeedTime.latest = new Date();
+									//loadFeedTime.oldest = new Date();
 									 
-								}else{
-									loadFeedTime.latest = feeds[0].get('updateTime');
-									loadFeedTime.oldest = feeds[(feeds.length)-1].get('updateTime');
-									user.set('loadFeedTime',loadFeedTime);
+								//}else{
+									//loadFeedTime.latest = feeds[0].get('updateTime');
+									//loadFeedTime.oldest = feeds[(feeds.length)-1].get('updateTime');
+									//user.set('loadFeedTime',loadFeedTime);
 									
-								}
-								user.save();
-								res.render('band', {
-									username: username,
-									groupObjId:groupObjId,
-									cnt:cnt,
-									isSignIn:isSignIn,
-									feeds:feeds
-								 });
-							});
+								//}
+								//user.save();
+								//res.render('band', {
+									//username: username,
+									//groupObjId:groupObjId,
+									//cnt:cnt,
+									//isSignIn:isSignIn,
+									//feeds:feeds
+								 //});
+							//});
 							
-						 });
+						 //});
 							 
 
-						 });
+						 //});
 						
-					  },
-					  error: function(object, error) {
-						// 失败了.
-					  }
-					});
-				 }
-			}); 
+					  //},
+					  //error: function(object, error) {
+						//// 失败了.
+					  //}
+					//});
+				 //}
+			//}); 
 		}else{
 				var username = result.data.openid;
 			 userclass.getCurrentGroup(username,function(err,whichGroupNow,whichGroupNameNow){
